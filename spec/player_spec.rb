79a75f752dd279b7d906ed2cc3cecdeb7bfd5776
Abigail <abigail.mcphillips@gmail.com>
2016-09-01 15:08:40 +0100

    refactoring Model layer

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -11,13 +11,21 @@ let(:name2) { double :name2 }
     expect(player1.name).to eq name1
   end
 
-  describe 'hit points' do
+  describe '#receive_damage' do
     it 'initially hit points are 100' do
       expect(player1.hit_points).to eq 100
     end
     it '#attack updates hit_points' do
-      expect{ player1.attack(player2) }.to change{player2.hit_points}.by(-10)
+      expect{ player2.receive_damage }.to change{player2.hit_points}.by(-10)
     end
   end
+
+  describe '#attack' do
+    it 'calls receive_damage' do
+      expect(player2).to receive(:receive_damage)
+      player1.attack(player2)
+    end
+  end
+
 
 end
